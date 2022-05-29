71503f391754a8abd97b6b78b07d956264cc9881
Jasper Palmer <jpalmer4@uw.edu>
2022-05-29 01:44:05 -0700

    Implement rough functional IntHistogram

--- src/java/simpledb/IntHistogram.java
+++ src/java/simpledb/IntHistogram.java
@@ -1,8 +1,16 @@
 package simpledb;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
 /** A class to represent a fixed-width histogram over a single integer-based field.
  */
 public class IntHistogram {
+    int[] hist;
+    int min;
+    int max;
 
     /**
      * Create a new IntHistogram.
@@ -21,7 +29,9 @@ public class IntHistogram {
      * @param max The maximum integer value that will ever be passed to this class for histogramming
      */
     public IntHistogram(int buckets, int min, int max) {
-    	// some code goes here
+        this.hist = new int[buckets];
+        this.min = min;
+        this.max = max;
     }
 
     /**
@@ -29,7 +39,8 @@ public class IntHistogram {
      * @param v Value to add to the histogram
      */
     public void addValue(int v) {
-    	// some code goes here
+        double range = (double) (max - min) / (hist.length - 1);
+        hist[(int) ((v - min) / range)]++;
     }
 
     /**
@@ -43,9 +54,36 @@ public class IntHistogram {
      * @return Predicted selectivity of this particular operator and value
      */
     public double estimateSelectivity(Predicate.Op op, int v) {
+        int totalElements = Arrays.stream(hist).sum();
+        double range = (double) (max - min) / (hist.length - 1);
+        int index = (int) ((v - min) / range);
 
-    	// some code goes here
-        return -1.0;
+        switch (op) {
+            case EQUALS:
+                if (index < 0 || index >= hist.length) return 0.0;
+                return (double) hist[index] / totalElements;
+            case GREATER_THAN:
+                if (index < 0) return 1.0;
+                if (index >= hist.length) return 0.0;
+                return (double) Arrays.stream(hist, index + 1, hist.length).sum() / totalElements;
+            case LESS_THAN:
+                if (index < 0) return 0.0;
+                if (index >= hist.length) return 1.0;
+                return (double) Arrays.stream(hist, 0, index).sum() / totalElements;
+            case LESS_THAN_OR_EQ:
+                if (index < 0) return 0.0;
+                if (index >= hist.length) return 1.0;
+                return (double) Arrays.stream(hist, 0, index + 1).sum() / totalElements;
+            case GREATER_THAN_OR_EQ:
+                if (index < 0) return 1.0;
+                if (index >= hist.length) return 0.0;
+                return (double) Arrays.stream(hist, index, hist.length).sum() / totalElements;
+            case NOT_EQUALS:
+                if (index < 0 || index >= hist.length) return 1.0;
+                return (double) (totalElements - hist[index]) / totalElements;
+            default:
+                return -1;
+        }
     }
     
     /**
